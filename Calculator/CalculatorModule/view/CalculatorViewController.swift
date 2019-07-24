0e24718308c07aab5f3955458beccf240ce123b3
Nelson <tomneo2004@gmail.com>
2019-07-24 16:46:03 +0800

    Add animation to button

--- Calculator/CalculatorModule/view/CalculatorViewController.swift
+++ Calculator/CalculatorModule/view/CalculatorViewController.swift
@@ -50,35 +50,88 @@ class CalculatorViewController : UIViewController, CalculatorControllerToViewPro
         print("View deinit")
     }
     
+    ///operator button pressed
+    ///only allow to be set nil and buttons that are
+    ///"x", "+", "-", "÷"
+    ///because these buttons cahnge their scale once they are pressed
+    ///and return original scale when other button pressed
+    ///this property is used to track which buttons "x", "+", "-", "÷" pressed
+    var operatorPressedButton : UIButton? {
+        didSet{
+            
+            if let oldValue = oldValue{
+                
+                CATransaction.begin()
+                
+                let springAnimate = CASpringAnimation(keyPath: "transform.scale")
+                springAnimate.duration = 0.1
+                
+                guard oldValue.layer.transform.m11 == oldValue.layer.transform.m22 else{
+                    fatalError("layer was not scale properly")
+                }
+                springAnimate.fromValue = oldValue.layer.transform.m11
+                springAnimate.toValue = 1.0
+                oldValue.layer.add(springAnimate, forKey: nil)
+                
+                CATransaction.setCompletionBlock {
+                    oldValue.layer.transform = CATransform3DMakeScale(1.0, 1.0, 1.0)
+                }
+                
+                CATransaction.commit()
+            }
+        }
+    }
+    
     //MARK: - Clear action
     
     @IBAction func clearPress(_ sender: UIButton) {
         
         MVC_Controller.onClearPress()
+        
+        operatorPressedButton = nil
+        buttonScaleAnim(sender)
     }
     
     //MARK: - Digital action
     @IBAction func digitalPress(_ sender: UIButton) {
         
         MVC_Controller.onDigitalNumberPress(sender.currentTitle!)
+        
+        operatorPressedButton = nil
+        buttonScaleAnim(sender)
     }
     
     //MARK: - Operator action
     @IBAction func operatorPress(_ sender: UIButton) {
         
         MVC_Controller.onOperatorPress(sender.currentTitle!)
+        
+        if sender.currentTitle == "+/-" || sender.currentTitle == "%"{
+            operatorPressedButton = nil
+            buttonScaleAnim(sender)
+        }
+        else{
+            operatorPressedButton = sender
+            buttonScaleAnim(sender, 1.0, 0.8, 0.8, false)
+        }
     }
     
     //MARK: - Calculate action
     @IBAction func calculatePress(_ sender: UIButton) {
         
         MVC_Controller.onCalculatePress()
+        
+        operatorPressedButton = nil
+        buttonScaleAnim(sender)
     }
     
     //MARK: - Decimal action
     @IBAction func decimalPress(_ sender: UIButton) {
         
         MVC_Controller.onDecimalPress()
+        
+        operatorPressedButton = nil
+        buttonScaleAnim(sender)
     }
     
     func onUpdateCalculatorDisplay(result: String) {
@@ -88,5 +141,28 @@ class CalculatorViewController : UIViewController, CalculatorControllerToViewPro
     
     func onUpdateCalculateSentence(result: String) {
         displaySentenceLabel.text = result
+    }
+}
+
+extension CalculatorViewController{
+    
+    ///Apply a spring animation to button
+    func buttonScaleAnim(_ button:UIButton, _ fromScale:CGFloat = 1.0, _ toScale:CGFloat = 0.8, _ settleScale:CGFloat = 1.0, _ autoreverses:Bool = true){
+        
+        CATransaction.begin()
+        
+        let springAnimate = CASpringAnimation(keyPath: "transform.scale")
+        springAnimate.duration = 0.1
+        springAnimate.fromValue = 1.0
+        springAnimate.toValue = 0.5
+        springAnimate.autoreverses = autoreverses
+        button.layer.add(springAnimate, forKey: nil)
+        
+        CATransaction.setCompletionBlock {
+            
+            button.layer.transform = CATransform3DMakeScale(settleScale, settleScale, 1.0)
+        }
+        
+        CATransaction.commit()
     }
 }
